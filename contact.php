6d171e150b0b75bbd8d912b75dae0994f4a76d3a
Gerard Knipper <gerard.knipper@gmail.com>
2015-06-05 11:53:30 +0200

    added contact.php

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>contact</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width">
+</head>
+<body>
+<div class="container">
+	<div class="header">
+<h1>Contact pagina</h1>
+	</div>
+	<div class="main">
+		<p>dit is een stukje tekst in de main section</p>
+		
+	</div>
+	<div class="footer">
+		
+		
+	</div>
+	
+</div>
+
+</body>
+</html>
